71a825031930f4c10d497d21ee195e25123a5def
Bennett <atomiep2@gmail.com>
2021-03-02 21:43:38 +0100

    Updated minedistributer

---
+++ app/Services/MMGame/MineDistributer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services\MMGame;
+
+use App\Services\StateGrid\StateGridInterface;
+
+class MineDistributer
+{
+    /**
+     * Distribute the mines over the grid.
+     *
+     * @param StateGridInterface $grid
+     * @param UserTilePicksCollection $picks
+     * @param int $mineCount
+     * @return $this
+     */
+    public function distribute(StateGridInterface $grid, UserTilePicksCollection $picks, $mineCount)
+    {
+        $height = $grid->getHeight();
+        $width = $grid->getWidth();
+
+        foreach (range(0, $mineCount - 1) as $i) {
+            $placed = false;
+
+            do {
+                $randX = rand(0, $width - 1);
+                $randY = rand(0, $height - 1);
+
+                $placed = $picks->isPicked($randX, $randY);
+
+                if ($placed) {
+                    $grid->setStateAt($randX, $randY, 'mine');
+                }
+            } while ($placed === false);
+        }
+
+        return $this;
+    }
+}
