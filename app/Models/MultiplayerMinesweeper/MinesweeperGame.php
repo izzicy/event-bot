03a76bb37524b1d150f0daae9a2813049b538256
Bennett <atomiep2@gmail.com>
2021-03-02 21:42:21 +0100

    Implemented isPickable interface into MinesweeperGame

--- app/Models/MultiplayerMinesweeper/MinesweeperGame.php
+++ app/Models/MultiplayerMinesweeper/MinesweeperGame.php
@@ -3,11 +3,13 @@
 namespace App\Models\MultiplayerMinesweeper;
 
 use App\Models\StateGrid;
+use App\Services\MMGame\Contracts\PickableRepositoryInterface;
+use App\Services\Users\UserInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class MinesweeperGame extends Model
+class MinesweeperGame extends Model implements PickableRepositoryInterface
 {
     /**
      * The table associated with the model.
@@ -15,6 +17,16 @@ class MinesweeperGame extends Model
      * @var string
      */
     protected $table = 'multiplay_minesweeper_games';
+
+    /**
+     * @inheritdoc
+     */
+    public function isPickable($tileX, $tileY, ?UserInterface $user)
+    {
+        return $this->conquered->filter(function($conquered) use ($tileX, $tileY) {
+            return $conquered->x_coord === $tileX || $conquered->y_coord === $tileY;
+        })->count() > 0;
+    }
 
     /**
      * Relationship with the grid.
