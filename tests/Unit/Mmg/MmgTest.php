22ca168e53972ade4ab65f483670afcb6f142f25
izzicy <atomiep2@gmail.com>
2021-04-21 20:09:30 +0200

    Started with mmg testcase

---
+++ tests/Unit/Mmg/MmgTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit\Mmg;
+
+use App\Mmg\Contracts\FactoryInterface;
+use App\Mmg\GameRepository;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class MmgTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function it_can_create_mmg_games()
+    {
+        $width = $this->faker->numberBetween(4, 5);
+        $height = $this->faker->numberBetween(4, 5);
+
+        $game = app(GameRepository::class)->create($width, $height);
+
+        $this->assertEquals($game->tiles->count(), $width * $height);
+
+        foreach ($game->tiles as $tile) {
+            $this->assertNull($tile->getConquerer());
+            $this->assertEquals('unknown', $tile->getState());
+            $this->assertEquals(0, count($tile->getFlaggers()));
+        }
+    }
+
+    /** @test */
+    public function it_successfully_distributes_mines_in_a_given_game()
+    {
+        $width = $this->faker->numberBetween(4, 5);
+        $height = $this->faker->numberBetween(4, 5);
+        $mineCount = $this->faker->numberBetween(4, 14);
+
+        $game = app(GameRepository::class)->create($width, $height);
+
+        /** @var FactoryInterface */
+        $factory = app(FactoryInterface::class);
+        $distributer = $factory->createMineDistributer([], $mineCount);
+
+        $distributer->operateGame($game);
+
+        $retrievedMineCount = 0;
+
+        foreach ($game->tiles as $tile) {
+            if ($tile->getState() === 'mine') {
+                $retrievedMineCount += 1;
+            }
+        }
+
+        $this->assertEquals($mineCount, $retrievedMineCount);
+    }
+}
