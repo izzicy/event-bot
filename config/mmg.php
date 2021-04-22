e9c1d7db8dc5f8b368df67f8d64dba551e7103da
izzicy <atomiep2@gmail.com>
2021-04-22 22:17:51 +0200

    Increased tile resolution

--- config/mmg.php
+++ config/mmg.php
@@ -4,7 +4,7 @@ return [
     'default-channel' => env('MULTIPLAYER_MINESWEEPER_DEFAULT_CHANNEL'),
     'test-channel' => env('MULTIPLAYER_MINESWEEPER_TEST_CHANNEL'),
 
-    'tile-size' => 32,
+    'tile-size' => 64,
 
     'tile-image-path' => storage_path('app/mmg/out-tile.png'),
     'mine-image-path' => storage_path('app/mmg/mine.png'),
