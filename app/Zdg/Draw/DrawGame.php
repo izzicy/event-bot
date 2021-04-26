fae985032edc192e383f879eff50e4ebdf25c4b8
izzicy <atomiep2@gmail.com>
2021-04-26 20:25:39 +0200

    Updated zdg drawing

--- app/Zdg/Draw/DrawGame.php
+++ app/Zdg/Draw/DrawGame.php
@@ -33,6 +33,60 @@ class DrawGame
             );
         });
 
-        return $canvas->getCore();
+        foreach (range(1, $width - 1) as $x) {
+            $canvas->rectangle($x * $pixelSize, 0, $x * $pixelSize, $height * $pixelSize, function ($draw) {
+                $draw->background('rgba(0, 0, 0, 0)');
+                $draw->border(1, 'rgba(0, 0, 0, 0.07)');
+            });
+        }
+
+        foreach (range(1, $height - 1) as $y) {
+            $canvas->rectangle(0, $y * $pixelSize, $width * $pixelSize, $y * $pixelSize, function ($draw) {
+                $draw->background('rgba(0, 0, 0, 0)');
+                $draw->border(1, 'rgba(0, 0, 0, 0.07)');
+            });
+        }
+
+        $outerCanvas = ImageManagerStatic::canvas(($width + 2) * $pixelSize, ($height + 2) * $pixelSize, '#36393F');
+
+        foreach (range(0, $width - 1) as $x) {
+            $outerCanvas->text($x + 1, round($pixelSize * 1.5) + $x * $pixelSize, round($pixelSize / 2), function($font) {
+                $font->file(config('zdg.font-path'));
+                $font->size(6);
+                $font->color('#dddddd');
+                $font->align('center');
+                $font->valign('middle');
+            });
+
+            $outerCanvas->text($x + 1, round($pixelSize * 1.5) + $x * $pixelSize, round($pixelSize * 1.5) + $height * $pixelSize, function($font) {
+                $font->file(config('zdg.font-path'));
+                $font->size(6);
+                $font->color('#dddddd');
+                $font->align('center');
+                $font->valign('middle');
+            });
+        }
+
+        foreach (range(0, $height - 1) as $y) {
+            $outerCanvas->text($height - $y, round($pixelSize / 2), round($pixelSize * 1.5) + $y * $pixelSize, function($font) {
+                $font->file(config('zdg.font-path'));
+                $font->size(6);
+                $font->color('#dddddd');
+                $font->align('center');
+                $font->valign('middle');
+            });
+
+            $outerCanvas->text($height - $y, round($pixelSize * 1.5) + $width * $pixelSize, round($pixelSize * 1.5) + $y * $pixelSize, function($font) {
+                $font->file(config('zdg.font-path'));
+                $font->size(6);
+                $font->color('#dddddd');
+                $font->align('center');
+                $font->valign('middle');
+            });
+        }
+
+        $outerCanvas->insert($canvas, 'top-left', $pixelSize, $pixelSize);
+
+        return $outerCanvas->getCore();
     }
 }
